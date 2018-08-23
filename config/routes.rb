6efbd12e2b8b0041e172a7fb5a582163ed9ed468
julius26 <juliusmeldrin@gmail.com>
2018-08-23 12:10:01 +0300

    routes changed

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # get 'static_pages/home'
   # staticpage home set to default
 
-  root  'static_pages#ghome'
+  root  'static_pages#home'
  
   get "static_pages/help", to: "static_pages#help"
 
